d6dbf2cd143f4cd67a4a9ffad175c95414bd0adc
ABUBAKAR MUHAMMAD DANHAYA <121393884+HABUDANHAYABK@users.noreply.github.com>
2025-08-16 07:52:23 +0100

    add new file

--- Introduction-to-python-programming/Variables-and-Functions/variables.py
+++ Introduction-to-python-programming/Variables-and-Functions/variables.py
@@ -1,3 +1,5 @@
 # collecting user name
 name = input("what's your name? ").strip().title()
-print(" Your name is ", name)
+print(f"Your name is {name}")
+# greating the user
+print(f"Good day {name}")
